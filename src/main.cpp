97a5c82e78463a4bd5283ab80eb0d338555e7c4b
viraltaco <vt_@gmx.us>
2022-04-13 23:11:28 +0200

    Fixed bug in issue #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 /// copyright 2019-2022 viraltaco_ <https://opensource.org/licenses/MIT>
-#include "mkpass.hpp"  // <string_view>, mkpass::{ kVersion,... }
+#include <random>    // std::{ random_device, default_random_engine,... }
+#include <cstdlib>   // std::{ strtoul, exit }
+#include <iostream>  // std::{ cout, clog, cerr,... }
 
-#include <cstdlib>     // std::{ strtoul, exit }
-#include <random>      // std::{ random_device, default_random_engine,... }
-#include <iostream>    // std::{ cout, clog, cerr,... }
+#include "mkpass.hpp"  // <string_view>, mkpass::{ kVersion,... }
 
 int main(int argc, char** argv) {
   std::cout.sync_with_stdio(false); // One thread. Nothing to sync.
   
-  auto passlen = 32UL;
+  auto passlen = 32ul;
   auto symbols = true;
   if (argc >= 2) {
     const auto kArg1 = std::string_view(argv[1]);
@@ -16,22 +16,24 @@ int main(int argc, char** argv) {
     switch (kArg1[0]) {
     case 'o':
       symbols = (kArg1[1] == 'n');
-      if (argc > 2) passlen = std::strtoul(argv[2], nullptr, 10);
+      if (argv[2]) // argv[argc] is always nullptr
+        passlen = std::strtoul(argv[2], nullptr, 10);
       break;
     case '-':
       if ("--help" != kArg1) {
         if ("--version" == kArg1)
-          std::cout << mkpass::kVersion;
+          std::cout << mkpass::kVersion << std::endl;
         else if ("--license" == kArg1)
-          std::cout << mkpass::kVersion << mkpass::kLicense;
+          std::cout << mkpass::kVersion << mkpass::kLicense << std::endl;
         else
           goto fail_;
         
         return EXIT_SUCCESS;
+      } else { // "--help" == kArg1
+        [[fallthrough]];
       }
-      [[fallthrough]];
     case '?':
-      std::cout << mkpass::kUsage;
+      std::cout << mkpass::kUsage << std::endl;
       return EXIT_SUCCESS;
 fail_:
     default:
@@ -42,17 +44,17 @@ fail_:
 
   using uchar = unsigned char;
   static constexpr uchar kAlphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-   "abcdefghijklmnopqrstuvwxyz0123456789!\\#$%&'( )*+,-./:;<=>?@[]^_{|}~\"";
+    "abcdefghijklmnopqrstuvwxyz0123456789!\\#$%&'( )*+,-./:;<=>?@[]^_{|}~\"";
 
   // length of kAlphabet (no symbols, no NUL) is 10 + 2(26) = 62
   const auto kLen = static_cast<uchar>((symbols) ? sizeof kAlphabet : 62u);
-  auto dist = std::uniform_int_distribution<uchar>(0u, kLen);
+  static auto dist = std::uniform_int_distribution<uchar>(0u, kLen - 1);
 
-  auto rice = std::random_device();
-  auto seed = std::seed_seq{ rice(), rice(), rice() };
-  auto mt32 = std::mt19937(seed);
-  const auto gen = [&] { return kAlphabet[dist(mt32)]; };
+  std::random_device rice;
+  std::seed_seq seed{ rice(), rice(), rice() };
+  static auto twister = std::mt19937_64(seed);
+  static const auto gen = [&] { return kAlphabet[dist(twister)]; };
 
   std::generate_n(std::ostream_iterator<uchar>(std::cout), passlen, gen);
-  std::cout.flush();
+  std::cout << std::endl;
 }

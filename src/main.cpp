7753d038b37468b14dfc3a3955e134ad62692b82
Anthony Capobianco <vt_@gmx.us>
2022-04-13 23:15:12 +0200

    issue #1 (#2)
    * Issue #1
    
    * Fixed bug in issue #1

--- src/main.cpp
+++ src/main.cpp
@@ -1,61 +1,60 @@
 /// copyright 2019-2022 viraltaco_ <https://opensource.org/licenses/MIT>
-#include "mkpass.hpp"  // <string_view>, mkpass::{ kVersion,... }
+#include <random>    // std::{ random_device, default_random_engine,... }
+#include <cstdlib>   // std::{ strtoul, exit }
+#include <iostream>  // std::{ cout, clog, cerr,... }
 
-#include <cstdlib>     // std::{ strtoul, exit }
-#include <random>      // std::{ random_device, default_random_engine,... }
-#include <iostream>    // std::{ cout, clog, cerr,... }
+#include "mkpass.hpp"  // <string_view>, mkpass::{ kVersion,... }
 
 int main(int argc, char** argv) {
   std::cout.sync_with_stdio(false); // One thread. Nothing to sync.
-
-  bool symbols = true;
-  if (argc >= 3) {
-    using mkpass::kVersion, mkpass::kLicense, mkpass::kUsage;
-    auto kArg1 = std::string_view(argv[1]);
+  
+  auto passlen = 32ul;
+  auto symbols = true;
+  if (argc >= 2) {
+    const auto kArg1 = std::string_view(argv[1]);
 
     switch (kArg1[0]) {
     case 'o':
       symbols = (kArg1[1] == 'n');
+      if (argv[2]) // argv[argc] is always nullptr
+        passlen = std::strtoul(argv[2], nullptr, 10);
       break;
     case '-':
       if ("--help" != kArg1) {
-        if ("--version" == kArg1) {
-          std::cout << kVersion << std::endl;
-          return EXIT_SUCCESS;
-        } else if ("--license" == kArg1) {
-          std::cout << kVersion << kLicense << std::endl;
-          return EXIT_SUCCESS;
-        } else {
-          std::cerr << kUsage;
-          return EXIT_FAILURE;
-        }
-      } else { // argv[1] == "--help"
+        if ("--version" == kArg1)
+          std::cout << mkpass::kVersion << std::endl;
+        else if ("--license" == kArg1)
+          std::cout << mkpass::kVersion << mkpass::kLicense << std::endl;
+        else
+          goto fail_;
+        
+        return EXIT_SUCCESS;
+      } else { // "--help" == kArg1
         [[fallthrough]];
       }
     case '?':
-      std::cout << kUsage << std::endl;
+      std::cout << mkpass::kUsage << std::endl;
       return EXIT_SUCCESS;
+fail_:
     default:
-      std::cerr << "https://youtu.be/tJjs4w_-lV4\n";
+      std::cerr << mkpass::kUsage << "\nhttps://youtu.be/tJjs4w_-lV4\n";
       return EXIT_FAILURE;
     }
   }
 
-  static constexpr unsigned char alphabet[] = {
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    "0123456789!\\#$%&'( )*+,-./:;<=>?@[]^_{|}~\""
-  };
+  using uchar = unsigned char;
+  static constexpr uchar kAlphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz0123456789!\\#$%&'( )*+,-./:;<=>?@[]^_{|}~\"";
 
-  // sizeof range_no_symbols_ignore_nul == 62u == 26 + 26 + 10;
-  const auto kRangeSize = static_cast<unsigned char>((symbols) ? sizeof alphabet : 62u);
-  std::random_device r;
-  std::seed_seq seed{r(), r(), r()};
-  static auto mt64 = std::mt19937(seed);
+  // length of kAlphabet (no symbols, no NUL) is 10 + 2(26) = 62
+  const auto kLen = static_cast<uchar>((symbols) ? sizeof kAlphabet : 62u);
+  static auto dist = std::uniform_int_distribution<uchar>(0u, kLen - 1);
 
-  static auto dist = std::uniform_int_distribution<unsigned char>(0u, kRangeSize);
-  static const auto gen = [&] { return alphabet[dist(mt64)]; };
+  std::random_device rice;
+  std::seed_seq seed{ rice(), rice(), rice() };
+  static auto twister = std::mt19937_64(seed);
+  static const auto gen = [&] { return kAlphabet[dist(twister)]; };
 
-  auto len = (argc >= 2) ? std::strtoul(argv[argc - 1], nullptr, 10) : 32UL;
-  std::generate_n(std::ostream_iterator<unsigned char>(std::cout), len, gen);
-  std::cout.flush();
+  std::generate_n(std::ostream_iterator<uchar>(std::cout), passlen, gen);
+  std::cout << std::endl;
 }
--- src/mkpass.hpp
+++ src/mkpass.hpp
@@ -3,49 +3,49 @@
 #include <string_view>
 
 namespace mkpass {
-  using std::string_view_literals::operator ""sv;
+using std::string_view_literals::operator ""sv;
 
-  static constexpr auto kVersion = "mkpass 1.3.5~Docs"sv;
-  static constexpr auto kLicense = R"(
-    Copyright (c) 2022 viraltaco_
-    
-    MIT License
-      Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-      The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    
-      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+static constexpr auto kVersion = "mkpass 1.4.2"sv;
+static constexpr auto kLicense = R"(
+Copyright (c) 2022 viraltaco_
+
+MIT License
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
   )"sv;
-  static constexpr auto kUsage = R"(
-   mkpass generates 'random' passwords.
-   usage:
-   mkpass [OPT] symbols length
-   
-   symbols         (off|on) if on use symbols. Default: on
-   length          (0 to 2^63) length/size in bytes. Default: 32
+static constexpr auto kUsage = R"(
+mkpass generates 'random' passwords.
+usage:
+mkpass [OPT] symbols length
 
-   OPT (one of):
-     --help, ?      Prints this help message.
-     --license      Prints the license.
-     --version      Prints the version.
-   
-   Examples:
-     mkpass         Prints a 32 characters long password with symbols.
-     mkpass on 32   Prints a 32 characters long password with symbols.
-     mkpass off 1   Prints one alpha-numeric character.
-     mkpass ?       Prints everything you just read.
+symbols         (off|on) if on use symbols. Default: on
+length          (0 to 2^63) length/size in bytes. Default: 32
+
+OPT (one of):
+  --help, ?      Prints this help message.
+  --license      Prints the license.
+  --version      Prints the version.
+
+Examples:
+  mkpass         Prints a 32 characters long password with symbols.
+  mkpass on 32   Prints a 32 characters long password with symbols.
+  mkpass off 1   Prints one alpha-numeric character.
+  mkpass ?       Prints everything you just read.
   )"sv;
 } // namespace mkpass
 #endif

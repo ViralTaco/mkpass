d3ebfb5b2fb439a6fa44176251cb58333c4ed749
viraltaco <vt_@gmx.us>
2022-04-25 18:07:03 +0200

    Always show version number. Amend formatted

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include "mkpass.hpp"// <string_view>, mkpass::{ kVersion,... }
 
 int main(int argc, char** argv) {
-  std::cout.sync_with_stdio(false);// One thread. Nothing to sync.
+  std::cout.sync_with_stdio(false); // One thread. Nothing to sync.
   using uchar = unsigned char;
   static constexpr uchar kAlphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
                                        "abcdefghijklmnopqrstuvwxyz0123456789"
@@ -18,27 +18,22 @@ int main(int argc, char** argv) {
     switch (const std::string_view kArg1(argv[1]); kArg1[0]) {
     case 'o':
       if (kArg1 == "off") { lastidx = lastidx - kSymCount; }
-      if (argv[2]) { passlen = std::strtoul(argv[2], nullptr, 10); }
+      if (argv[2])        { passlen = std::strtoul(argv[2], nullptr, 10); }
       break;
     case '-':
       std::cout << mkpass::kVersion;
-      if (kArg1 == "--license") {
-        std::cout << mkpass::kLicense;
-      } else if (kArg1 != "--version") {
-        std::cout << mkpass::kUsage;
-      }
+      if      (kArg1 == "--license") { std::cout << mkpass::kLicense; }
+      else if (kArg1 != "--version") { std::cout << mkpass::kUsage; }
       [[fallthrough]];
-    default: std::cout.put('\n'); return EXIT_SUCCESS;
+    default:
+      std::cout.put('\n');
+      return EXIT_SUCCESS;
     }
   }
   static std::uniform_int_distribution<uchar> dist(0u, lastidx);
 
   std::random_device r;
-  std::seed_seq seed{
-    r(),
-    r(),
-    r(),
-  };
+  std::seed_seq seed{ r(), r(), r(), };
   static auto twister = std::mt19937_64(seed);
 
   std::generate_n(std::ostream_iterator<uchar>(std::cout), passlen,
